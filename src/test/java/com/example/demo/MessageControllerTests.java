2eaafd44647ef656a270a00680d386e6943615a6
Justina Andrulionyte <69106997+voidbeary@users.noreply.github.com>
2022-03-10 14:29:55 +0200

    fix test

--- src/test/java/com/example/demo/MessageControllerTests.java
+++ src/test/java/com/example/demo/MessageControllerTests.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -30,11 +29,12 @@ public class MessageControllerTests {
     @Test
     public void shouldBEandDBWork() {
         when(repository.findAll()).thenReturn(Arrays.asList(new Message(MESSAGE)));
-        assertEquals(MessageController.BE_SUCCESS_MESSAGE + MessageController.DB_SUCCESS_MESSAGE + MESSAGE, controller.helloWorld());
+        assertEquals(MessageController.BE_SUCCESS_MESSAGE + MessageController.DB_SUCCESS_MESSAGE + MESSAGE, controller.getMessage());
     }
+
     @Test
     public void shouldWorkOnlyBE() {
         when(repository.findAll()).thenReturn(new ArrayList<>());
-        assertEquals(MessageController.BE_SUCCESS_MESSAGE, controller.helloWorld());
+        assertEquals(MessageController.BE_SUCCESS_MESSAGE, controller.getMessage());
     }
 }
